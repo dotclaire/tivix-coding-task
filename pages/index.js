01a773c4c213349b50d54a533b38c4b049e6cee5
Charikleia Charalampidou <cl.charal@gmail.com>
2020-07-16 11:36:38 +0100

    Add input form

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,33 @@
 import React, { useEffect, useState } from "react";
 
+import { Form } from "../components/Form";
 import Head from "next/head";
 import axios from "axios";
 
 export default function Home() {
   const [data, setData] = useState();
+  const [input, setInput] = useState();
+  const [error, setError] = useState();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const API_KEY = "9b5596f8ec5036640bf15d987c734bcd";
+  const handleChange = event => {
+    event.preventDefault();
+    setInput(event.target.value);
+  };
 
-      try {
-        const result = await axios.get(
-          `https://api.openweathermap.org/data/2.5/forecast?q=London,UK&appid=${API_KEY}`
-        );
+  const handleSubmit = async event => {
+    event.preventDefault();
+    const API_KEY = "9b5596f8ec5036640bf15d987c734bcd";
 
-        setData(result.data);
-      } catch (error) {
-        error.message;
-      }
-    };
+    try {
+      const res = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${input}&appid=${API_KEY}`
+      );
 
-    fetchData();
-  }, []);
+      setData(res.data);
+    } catch (error) {
+      setError(error);
+    }
+  };
 
   return (
     <div>
@@ -31,13 +36,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {data && (
-        <div>
-          <h1>{data.city.name}</h1>
-          <h2>Temperature</h2>
-          <p>Max: {data.list[0].main.temp_max}</p>
-          <p>Min: {data.list[0].main.temp_min}</p>
-        </div>
+      {error && <div>{error.message}</div>}
+      {!error && (
+        <Form handleSubmit={handleSubmit} handleChange={handleChange} />
       )}
     </div>
   );
