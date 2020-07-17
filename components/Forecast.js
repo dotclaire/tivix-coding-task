5c5babcdac04db9952634e9d7c5f1472c3151187
Charikleia Charalampidou <cl.charal@gmail.com>
2020-07-17 10:51:35 +0100

    Format hour and add description

--- components/Forecast.js
+++ components/Forecast.js
@@ -15,7 +15,7 @@ export const Forecast = ({ data }) => {
     return groupedForecasts;
   }, {});
 
-  return data ? (
+  return (
     <div className="flex flex-col p-6">
       <div className="flex justify-center">
         <h1 className="text-xl font-semibold">Forecast for {data.city.name}</h1>
@@ -23,23 +23,30 @@ export const Forecast = ({ data }) => {
       <Carousel showThumbs={false}>
         {Object.keys(groupedForecasts).map((el, i) => {
           return (
-            <div className="bg-primary p-6" key={i}>
+            <div className="bg-primary pt-6 pb-10 px-6" key={i}>
               <h1 className="text-lg text-black">{el}</h1>
               <div className="flex flex-col lg:flex-row justify-between">
-                {groupedForecasts[el].map((d, i) => (
-                  <div key={i}>
-                    <h4 className="text-black">{d.dt_txt.split(" ")[1]}</h4>
-                    <span className="inline-block text-black">
-                      {getCelsius(d.main.temp).toFixed(2)}
-                      &#8451;
-                    </span>
-                  </div>
-                ))}
+                {groupedForecasts[el].map((d, i) => {
+                  const completeHours = d.dt_txt.split(" ")[1];
+                  const hours = `${completeHours.split(":")[0]}:${
+                    completeHours.split(":")[1]
+                  }`;
+                  return (
+                    <div className="m-4 lg:m-0" key={i}>
+                      <h4 className="text-black">{hours}</h4>
+                      <p>{d.weather[0].description}</p>
+                      <span className="inline-block text-black">
+                        {getCelsius(d.main.temp).toFixed(0)}
+                        &#8451;
+                      </span>
+                    </div>
+                  );
+                })}
               </div>
             </div>
           );
         })}
       </Carousel>
     </div>
-  ) : null;
+  );
 };
