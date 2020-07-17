51e4778ed6db69742d8167440e5ad007c017d017
Charikleia Charalampidou <cl.charal@gmail.com>
2020-07-17 15:39:15 +0100

    Remove pages indicators from carousel

--- components/Forecast.js
+++ components/Forecast.js
@@ -15,42 +15,44 @@ export const Forecast = ({ error, data }) => {
     return groupedForecasts;
   }, {});
 
+  if (error) {
+    return null;
+  }
+
   return (
-    !error && (
-      <div className="flex flex-col p-6">
-        <div className="flex justify-center">
-          <h1 className="p-4 text-xl font-semibold">
-            Forecast for {data.city.name}
-          </h1>
-        </div>
-        <Carousel showThumbs={false}>
-          {Object.keys(groupedForecasts).map((el, i) => {
-            return (
-              <div className="bg-primary h-full pt-6 pb-10 px-6" key={i}>
-                <h1 className="pb-4 text-lg text-black">{el}</h1>
-                <div className="flex flex-col lg:flex-row justify-between">
-                  {groupedForecasts[el].map((d, i) => {
-                    const completeHours = d.dt_txt.split(" ")[1];
-                    const hours = `${completeHours.split(":")[0]}:${
-                      completeHours.split(":")[1]
-                    }`;
-                    return (
-                      <div className="m-4 lg:m-0" key={i}>
-                        <h4 className="text-black font-semibold">{hours}</h4>
-                        <p>{d.weather[0].description}</p>
-                        <span className="inline-block text-black">
-                          {getCelsius(d.main.temp).toFixed(0)}
-                          &#8451;
-                        </span>
-                      </div>
-                    );
-                  })}
-                </div>
-              </div>
-            );
-          })}
-        </Carousel>
+    <div className="flex flex-col p-6">
+      <div className="flex justify-center">
+        <h1 className="p-4 text-xl font-semibold">
+          Forecast for {data.city.name}
+        </h1>
       </div>
-    )
+      <Carousel showThumbs={false}>
+        {Object.keys(groupedForecasts).map((el, i) => {
+          return (
+            <div className="bg-primary h-full pt-6 pb-10 px-6" key={i}>
+              <h1 className="pb-4 text-lg text-black">{el}</h1>
+              <div className="flex flex-col lg:flex-row justify-between">
+                {groupedForecasts[el].map((d, i) => {
+                  const completeHours = d.dt_txt.split(" ")[1];
+                  const hours = `${completeHours.split(":")[0]}:${
+                    completeHours.split(":")[1]
+                  }`;
+                  return (
+                    <div className="m-4 lg:m-0" key={i}>
+                      <h4 className="text-black font-semibold">{hours}</h4>
+                      <p>{d.weather[0].description}</p>
+                      <span className="inline-block text-black">
+                        {getCelsius(d.main.temp).toFixed(0)}
+                        &#8451;
+                      </span>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </Carousel>
+    </div>
   );
 };
